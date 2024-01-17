aaeaa21130f596b86eb1971b655dc7741d322c60
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-01-17 11:44:47 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Use the prefix for GitHub Pages
-  basePath: '/nextjs-github-actions',
-  assetPrefix: '/nextjs-github-actions/',
-  // Enable static export
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  output: 'export',
+  // reactStrictMode: true,
+  // swcMinify: true,
+  // // Use the prefix for GitHub Pages
+  // basePath: '/nextjs-github-actions',
+  // assetPrefix: '/nextjs-github-actions/',
+  // // Enable static export
+  // trailingSlash: true,
+  // images: {
+  //   unoptimized: true
+  // },
+  // output: 'export',
 }
 
 module.exports = nextConfig
